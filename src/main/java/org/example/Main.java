9f07e626a14b593fab08afb6c9aeb97582b089a2
Hozan9 <hozan.bahaden.93@gmail.com>
2023-03-03 12:26:14 +0100

    First Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 12;
+        if (i < 10) {
+            System.out.println("i<10");
+
+        } else {
+            System.out.println("i>10");
+        }
+
+
+        for (int a = 0; a < 3; a++) {
+            System.out.println("Hello");
+        }
+    }
+}
